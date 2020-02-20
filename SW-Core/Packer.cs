1f7fc8e045032b2283c71526a048ac43bec6e9cc
Sergey Sergeyich <theases150@gmail.com>
2020-02-20 19:46:39 +0300

    Added functional for creating a samplepack directories

--- SW-Core/Packer.cs
+++ SW-Core/Packer.cs
@@ -2,9 +2,42 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+
 namespace SW_Core.Watchers
 {
+    using DIR = System.IO.Directory;
     class Packer
     {
+        private string directory = "";
+        public string Name { get; private set; }
+        public string Directory
+        {
+            get => directory;
+            private set
+            {
+                DIR.CreateDirectory(value);
+
+                directory = value;
+            }
+        }
+
+        public Packer(string fullDestinationDirectory)
+        {
+            Directory = fullDestinationDirectory;
+
+            Console.WriteLine("Directory for pack created!");
+        }
+
+        public Packer(string destinationDirectory, string packName)
+        {
+            Name = packName != ""
+                ? packName.Trim('\\')
+                : throw new ArgumentNullException();
+
+            Directory = destinationDirectory.TrimEnd('\\') + '\\' + packName;
+
+            Console.WriteLine("Directory for pack created!");
+        }
     }
 }
